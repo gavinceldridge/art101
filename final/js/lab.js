5b30bc966c66631a9e633196289cf40b5d3a03ed
EvanRobel <Evanrobel@gmail.com>
2022-12-16 16:59:17 -0800

    yuh
    yh

--- final/js/lab.js
+++ final/js/lab.js
@@ -1,16 +1,35 @@
+
+
+
+let saturationVal = 0;
+let exposureVal = 0;
+let contrastVal = 0;
+let brightnessVal = 0;
+let hueVal = 0;
+
+
 // taken from https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL#javascript
 // turns the given file into a picture
 function previewFile() {
-  const preview = document.querySelector('img');
+  //removes old images if replacing
+  console.log("delete");
+  $('#outputimg').remove();
+  $('#inputimg').remove();
+  //const preview = document.getElementById('outputimg');
   const file = document.querySelector('input[type=file]').files[0];
   const reader = new FileReader();
   reader.addEventListener("load", function () {
     // convert image file to base64 string
     //After Image
-    preview.src = reader.result;
+    //creates img elelment
+    var afterImgElement = document.createElement('img');
+    afterImgElement.setAttribute("id","outputimg");
+    afterImgElement.src = reader.result;
+    document.getElementById('after').appendChild(afterImgElement);
     //Before Image
     //creates img elelment
     var beforeImgElement = document.createElement('img');
+    beforeImgElement.setAttribute("id","inputimg");
     //changes the src of the element to uploaded file src
     beforeImgElement.src = reader.result;
     //appends new img element to before div
@@ -21,37 +40,21 @@ function previewFile() {
   }
 }
 
-//before image diplay
+
 
 
 // A function added to the randomizer button that adds all the randomized elements
-$("#haveImageButton").click(function(){
+$("#imageInput").on("input", (function(){
 
     // sets the img into a canvas that can now be used to be editing, as well as resize the image keeping the original ratio
 
     Caman("#outputimg", function () {
     // resize the img
     this.resize({
-      width: 600
-      //width: (this.width /4)
+      //width: 600
     });
     this.render()
 });
-
-// appends the rest of the randomizer sliders
-  $(this).parent().append("<p> Saturation Slider </p>");
-  $(this).parent().append('<input type ="range" min = "-100" max="100" step ="1" value="0"  id="img_Saturation">');
-  $(this).parent().append('<p> Exposure Slider </p>');
-  $(this).parent().append('<input type ="range" min = "-100" max="100" step ="1" value="0"  id="img_Exposure">');
-  $(this).parent().append('<p> Contrast Slider </p>');
-  $(this).parent().append('	 <input type ="range" min = "-100" max="100" step ="1" value="0"  id="img_Contrast">');
-  $(this).parent().append('<p> Shadow Slider </p>');
-  $(this).parent().append('		 <input type ="range" min = "-100" max="100" step ="1" value="0"  id="img_brightness">');
-  $(this).parent().append('<p> Highlight Slider </p>');
-  $(this).parent().append('		 <input type ="range" min = "-100" max="100" step ="1" value="0"  id="img_hue">');
-
-  // randomizer button
-  $(this).parent().append('	<p>	 <button id = "randomize_me"> Press me apply changes </button> </p>');
 
 
 
@@ -62,35 +65,189 @@ $("#haveImageButton").click(function(){
   var pic_contrast = $("#img_Contrast");
   var pic_brightness = $("#img_brightness");
   var pic_hue = $("#img_hue");
-  console.log(pic_saturation)
+
+ $("#img_Saturation").on("input", function(){
+  $("#Saturation_val").html(pic_saturation.val());
+  saturationVal = pic_saturation.val();
+ });
+
+ $("#img_Exposure").on("input", function(){
+  $("#Exposure_val").html(pic_exposure.val());
+  exposureVal = pic_exposure.val();
+ });
+
+ $("#img_brightness").on("input", function(){
+  $("#Brightness_Val").html(pic_brightness.val());
+  brightnessVal = pic_brightness.val();
+ });
+
+ $("#img_Contrast").on("input", function(){
+  $("#Contrast_Val").html(pic_contrast.val());
+  contrastVal = pic_contrast.val();
+ });
+
+ $("#img_hue").on("input", function(){
+  $("#Hue_Val").html(pic_hue.val());
+  hueVal = pic_hue.val();
+ });
 
 
-// changes variables to slider
-// Currently Testing since it dosen't work, thats why I just put the variables to 50 except saturation for testing purposes
-  $("#randomize_me").click(function(){
-    Caman("#outputimg", function () {
-    this.saturation(pic_saturation.val());
-    this.exposure(pic_exposure.val());
-    this.contrast(pic_contrast.val());
-    this.brightness(pic_brightness.val());
-    this.hue(pic_hue.val());
+
+// changes variables to slider #applyButton
+$("#applyButton").click(function(){
+  //removes old AFTER image element
+  $('#outputimg').remove();
+  $('#after').find('img').remove();
+  console.log("reset");
+  //creates new AFTER image element
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+  reader.addEventListener("load", function () {
+    var afterImgElement = document.createElement('img');
+    afterImgElement.setAttribute("id","outputimg");
+    afterImgElement.src = reader.result;
+    document.getElementById('after').appendChild(afterImgElement);
+    //calls apply changes function
+    applyChanges();
+  }, false);
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+})
+
+
+
+//apply settings changes
+function applyChanges() {
+  Caman("#outputimg", function () {
+    //sets image propertie
+    this.contrast(contrastVal/4);
+    this.saturation(saturationVal/2);
+    this.exposure(exposureVal/2);
+    this.brightness(brightnessVal/2);
+    this.hue(hueVal/4);
     this.render()
+});
+};
+
+
+//randomize function
+
+// on click, randomizes picture
+$("#random_var").click(function(){
+  //removes old AFTER image element
+  $('#outputimg').remove();
+  $('#after').find('img').remove();
+  console.log("reset");
+  //creates new AFTER image element
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+  reader.addEventListener("load", function () {
+    var afterImgElement = document.createElement('img');
+    afterImgElement.setAttribute("id","outputimg");
+    afterImgElement.src = reader.result;
+    document.getElementById('after').appendChild(afterImgElement);
+    //calls randomize function
+    randomize();
+  }, false);
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+})
+
+//randomize function
+function randomize() {
+  //creates random integer vars for each value
+  saturationVal = Math.round(Math.random() * (100 - (-100))+(-100));
+  exposureVal = Math.round(Math.random() * (100 - (-100))+(-100));
+  contrastVal = Math.round(Math.random() * (100 - (-100))+(-100));
+  brightnessVal = Math.round(Math.random() * (100 - (-100))+(-100));
+  hueVal = Math.round(Math.random() * (100 - (-100))+(-100));
+  //puts value on slider
+  $("#Saturation_val").html(saturationVal);
+  $("#Exposure_val").html(exposureVal);
+  $("#Contrast_Val").html(contrastVal);
+  $("#Brightness_Val").html(brightnessVal);
+  $("#Hue_Val").html(hueVal);
+  //sets slider location to value
+  //saturation
+  saturationSlider = document.getElementById("img_Saturation");
+  saturationSlider.value = saturationVal;
+
+  //Exposure
+  exposureSlider = document.getElementById("img_Exposure");
+  exposureSlider.value = exposureVal;
+
+  //Contrast
+  contrastSlider = document.getElementById("img_Contrast");
+  contrastSlider.value = contrastVal;
+
+  //birhgtness
+  brightnessSlider = document.getElementById("img_brightness");
+  brightnessSlider.value = brightnessVal;
+
+  //Hue
+  HueSlider = document.getElementById("img_hue");
+  HueSlider.value = hueVal;
+
+
+  //calls apply changes function
+  applyChanges();
+};
+
+//downlaod
+// let downloadButton = document.getElementById("download");
+// let img = document.getElementById("outputimg");
+//
+//
+// downloadButton.addEventListener('click', () => {
+//   let imgPath = img.getAttribute('src');
+//   let filename = getFileName(imgPath);
+//
+//   saveas(imgPath, filename);
+// });
+//
+// function getFileName(str){
+//   return str.substring(str.last.lastIndexOf('/')+1);
+// }
+
+
+let downloadLink = '';
+$('#download').click(function(){
+Caman("#outputimg", function () {
+  downloadLink = document.getElementById('outputimg').src;
+  //var downloadLink = $('img#outputimg').src;
+  console.log(downloadLink);
+  this.render(function () {
+    this.save(downloadLink);
   });
 });
 
+})
 
-// adds randomization button (Works!)
-  $(this).parent().append('<p>	 <button id = "random_var"> Randomize </button> </p>');
-  // on click, randomizes picture
-  $("#random_var").click(function(){
-    Caman("#outputimg", function () {
-    this.saturation(Math.random() * (50 - (-50))+50);
-    this.exposure(Math.random() * (10 - (-10))+10);
-    this.contrast(Math.random() * (5 - (-5)) + 5);
-    this.brightness(Math.random() * (10 - (-10)) + 10);
-    this.hue(Math.random() * (20 - (-20)) + 20);
-    this.render()
-  });
+
+
+
+//buton coldowns
+
+//random button cooldown
+$('#random_var').click(function(){
+  var btn = $(this);
+  btn.prop('disabled', true);
+  setTimeout(function(){
+    btn.prop('disabled', false);
+  },500);
 });
 
-  });
+// apply changes cool down
+$('#applyButton').click(function(){
+  var btn = $(this);
+  btn.prop('disabled', true);
+  setTimeout(function(){
+    btn.prop('disabled', false);
+  },500);
+});
+
+
+
+}));
